1538d852078bd74519952d9c50c1ddb01ad97149
Angélica Silva <angelicarocha67@yahoo.com.br>
2020-03-30 00:28:20 -0300

    Melhorias diferentes da apostila

--- Lab01Cap02.ADONet.Web/Lab01Cap02.ADONet.Web/App_Start/RouteConfig.cs
+++ Lab01Cap02.ADONet.Web/Lab01Cap02.ADONet.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Lab01Cap02.ADONet.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "ListaTarefas", id = UrlParameter.Optional }
             );
         }
     }
